521855e4c893e21db4d7f6289d6ba5d36c34e13a
Joao Valle <gustojvalle@outlook.com>
2020-12-30 15:32:16 +0000

    database agin

--- controllers/signin.js
+++ controllers/signin.js
@@ -8,8 +8,9 @@
        }
     db('login').select('email', 'hasg').from("login").where("email", "=", email)
     .then(data => {
+            console.log(data[0])
             if(data[0]){
-            bcrypt.compare(password, data[0].hash , function(err, res){
+            bcrypt.compare(password, data[0].hasg , function(err, res){
                 console.log(res);
                 if(res){
                     db.select('*').from('users').where("email", "=", email)
